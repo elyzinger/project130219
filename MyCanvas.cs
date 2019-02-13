448ebfc7fde501d02f9f83e7ef7c350e84e195e3
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-02-13 20:53:48 +0200

    Update MyCanvas.cs

--- MyCanvas.cs
+++ MyCanvas.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +8,31 @@ namespace CanvasProject
 {
     public class MyCanvas
     {
-        public const int  MaxWidth = 800;
+        public const int MaxWidth = 800;
         public const int MaxHEight = 600;
         private static int buttonIndex = 0;
-        private static int button[] = new button[MaxButton];
-        private static int MaxButton = 3; 
-        public static bool CreateNewButton(x1, x2, y1, y2)
+        private static MyButton[] buttons= new MyButton[MaxButtons];
+        private static int MaxButtons = 3;
+        public static bool CreateNewButton(int x1,int x2,int y1,int y2)
         {
-
+            bool result = true;
+            Point topleft = new Point(x1, y2);
+            Point bottomright = new Point(x2, y2);
+            if (buttonIndex != MaxButtons && topleft.GetX() < MaxWidth && bottomright.GetX() < MaxWidth  && topleft.GetY() < MaxHEight  && bottomright.GetY() < MaxHEight)
+            {
+                buttons[buttonIndex] = new MyButton(topleft, bottomright);
+                buttonIndex++;           
+                
+            }
+            else 
+            {
+                result = false;
+            }
+            return result;
         }
         public static bool MoveButton(int buttonNumber, int x, int y)
         {
-
+           for
         }
         public static bool DeleteLastButton()
         {
@@ -28,7 +41,7 @@ namespace CanvasProject
         public static void ClearAllButtons()
         {
 
-        } 
+        }
         public static int GetCurrentNumberOfButtons()
         {
 
