9fa597c26c4c773c968f005d69faac375e404f95
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-02-16 20:42:03 +0200

    Update MyCanvas.cs

--- MyCanvas.cs
+++ MyCanvas.cs
@@ -4,80 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CanvasProject
+namespace MyCanvas
 {
-    public class MyCanvas
+    class MyCanvas
     {
-        public const int MaxWidth = 800;
-        public const int MaxHEight = 600;
-        private static int buttonIndex = 0;
-        private static MyButton[] buttons= new MyButton[MaxButtons];
-        private static int MaxButtons = 3;
-        public static bool CreateNewButton(int x1,int x2,int y1,int y2)
+        public const int maxwidth = 800;
+        public const int maxheight = 600;
+        private static int buttonindex = 0;
+        private static MyButton[] buttons = new MyButton[maxbuttons];
+        private static int maxbuttons = 3;
+
+        public static bool CreateNewButton(int x1, int x2,int y1, int y2)
         {
-            bool result = true;
-            Point topleft = new Point(x1, y2);
+            bool result = false;
+            Point topleft = new Point(x1, x2);
             Point bottomright = new Point(x2, y2);
-            if (buttonIndex != MaxButtons && topleft.GetX() < MaxWidth && bottomright.GetX() < MaxWidth  && topleft.GetY() < MaxHEight  && bottomright.GetY() < MaxHEight)
+            if (buttonindex < maxbuttons && topleft.GetX()<maxwidth && topleft.GetY()<maxheight && bottomright.GetX()<maxwidth && bottomright.GetY() < maxheight)
             {
-                buttons[buttonIndex] = new MyButton(topleft, bottomright);
-                buttonIndex++;           
-                
-            }
-            else 
-            {
-                result = false;
+                buttons[buttonindex] = new MyButton(topleft, bottomright);
+                buttonindex++;
+                result = true;
             }
             return result;
         }
         public static bool MoveButton(int buttonNumber, int x, int y)
         {
-           bool result;
-            if (buttonIndex == 0)
+            bool result;
+            if(buttonNumber <= 0 || buttonNumber > buttonindex)
             {
                 result = false;
             }
-
-
-
+            else
+            {
+                int newTopLeftX = buttons[buttonNumber].GetTopLeft().GetX() + x;
+                int newTopLeftY = buttons[buttonNumber].GetTopLeft().GetY() + y;
+                int newBottomRightX = buttons[buttonNumber].GetTopLeft().GetX() + x;
+                int newBottomRightY = buttons[buttonNumber].GetTopLeft().GetY() + y;
+                Point newTopLeft = new Point(newTopLeftX, newTopLeftY);
+                Point newBottomRight = new Point(newBottomRightX, newBottomRightY);
+                buttons[buttonNumber] = new MyButton(newTopLeft, newBottomRight);
+                result = true;
+            }
             return result;
-        }
-        public static bool DeleteLastButton()
-        {
-
-        }
-        public static void ClearAllButtons()
-        {
-
-        }
-        public static int GetCurrentNumberOfButtons()
-        {
-
-        }
-        public static int GetMaxNumberOfButtons()
-        {
-
-        }
-        public static int GetTheMaxWidthOfButtons()
-        {
-
-        }
-        public static int GetTheMaxHeightOfButtons()
-        {
-
-        }
-        public static bool IsPointInsideAButton(x, y)
-        {
-
-        }
-        public static bool CheckIfAnyButtonIsOverlaping()
-        {
-
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
         }
     }
 }
