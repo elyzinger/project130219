8805b2c06a9a83dde093999485919e55ce81adbe
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-02-16 22:33:52 +0200

    Update MyCanvas.cs

--- MyCanvas.cs
+++ MyCanvas.cs
@@ -14,12 +14,12 @@ namespace MyCanvas
         private static MyButton[] buttons = new MyButton[maxbuttons];
         private static int maxbuttons = 3;
 
-        public static bool CreateNewButton(int x1, int x2,int y1, int y2)
+        public static bool CreateNewButton(int x1, int x2, int y1, int y2)
         {
             bool result = false;
             Point topleft = new Point(x1, x2);
             Point bottomright = new Point(x2, y2);
-            if (buttonindex < maxbuttons && topleft.GetX()<maxwidth && topleft.GetY()<maxheight && bottomright.GetX()<maxwidth && bottomright.GetY() < maxheight)
+            if (buttonindex < maxbuttons && topleft.GetX() < maxwidth && topleft.GetY() < maxheight && bottomright.GetX() < maxwidth && bottomright.GetY() < maxheight)
             {
                 buttons[buttonindex] = new MyButton(topleft, bottomright);
                 buttonindex++;
@@ -30,7 +30,7 @@ namespace MyCanvas
         public static bool MoveButton(int buttonNumber, int x, int y)
         {
             bool result;
-            if(buttonNumber <= 0 || buttonNumber > buttonindex)
+            if (buttonNumber <= 0 || buttonNumber > buttonindex)
             {
                 result = false;
             }
@@ -47,5 +47,98 @@ namespace MyCanvas
             }
             return result;
         }
+        public static bool DeletLastButton()
+        {
+            bool result;
+            if (buttonindex == 0)
+            {
+                result = false;
+            }
+            else
+            {
+                buttonindex--;
+                result = true;
+            }
+            return result;
+        }
+        public static void ClearAllButtons()
+        {
+            while (buttonindex != 0)
+            {
+                buttonindex--;
+            }
+        }
+        public static int GetCurrentNumberOfButtons()
+        {
+            return buttonindex;
+        }
+        public static int GetMaxNumberOfButoons()
+        {
+            return maxbuttons;
+        }
+        public static int GetTheMaxWidth()
+        {
+            int maxwidth = 0, result = 0;
+            if (buttonindex == 0)
+            {
+                result = 0;
+            }
+            else
+            {
+                for(int i = 0; i < buttons.Length; i++)
+                {
+                    if (buttons[i].GetWidth() > maxwidth)
+                        maxwidth = buttons[i].GetWidth();
+                }
+                    
+            }
+            result = maxwidth;
+            return result;                      
+        }
+        public static int GetMaxHeightOfButtons()
+        {
+            int maxheight = 0, result = 0;
+            if (buttonindex == 0)
+            {
+                result = 0;
+            }
+            else
+            {
+                for (int i = 0; i < buttons.Length; i++)
+                {
+                    if (buttons[i].GetHeight() > maxheight)
+                        maxheight = buttons[i].GetHeight(); 
+                }
+
+            }
+            result = maxheight;
+            return result;
+        }
+        public static bool IsPonitInsideAButton(int x, int y)
+        {
+            bool result = false;
+            foreach(MyButton button in buttons)
+            {
+                if(x > button.GetTopLeft().GetX() && y < button.GetTopLeft().GetY() && x < button.GetBottomRight().GetX() && y > button.GetBottomRight().GetY())
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+        public static bool CheckIfAnyButtonOverLapping()
+        {
+            bool result = false;
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if(IsPonitInsideAButton(buttons[i].GetTopLeft().GetX(),buttons[i].GetTopLeft().GetY()) || IsPonitInsideAButton(buttons[i].GetBottomRight().GetX(), buttons[i].GetBottomRight().GetY()))
+                { 
+                    result = true;
+                }
+
+            }
+            return result;
+        }
+
     }
 }
